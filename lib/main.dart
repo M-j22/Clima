6af730786b539f452140cf5f9d55007d1de770ad
Mohit <jindalmohit22@gmail.com>
2022-08-23 09:04:36 +0530

    resolved all the errors

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:clima/screens/loading_screen.dart';
+import 'package:flutter_config/flutter_config.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await FlutterConfig.loadEnvVariables();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: LoadingScreen(),
+    );
+  }
+}
